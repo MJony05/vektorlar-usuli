e698cf7fb38fb89975f701bf4309b392e7d9c439
Jonibek <94430009+MJony05@users.noreply.github.com>
2022-05-15 18:14:34 +0300

    created repo

---
+++ script.js
@@ -0,0 +1,31 @@
+let a = 2,
+  b = 3;
+const func = (x) => {
+  return x * x * x - 5 * x - Math.cos(x);
+};
+let xn = 2;
+console.log("x0=" + xn);
+for (let i = 0; i < 100; i++) {
+  let xnn = xn - (func(xn) * (b - xn)) / (func(b) - func(a));
+  if (xnn - xn > 0.001) {
+    xn = xnn;
+    console.log("x" + (i + 1) + "=" + xnn);
+  } else break;
+}
+
+/*
+let a = 0,
+  b = 4,
+  n = 3;
+let h = (b - a) / n;
+let S = 0;
+for (let i = 1; i <= n - 1; i++) {
+  let x = a + i * h;
+  S += 1 / (1 + Math.sqrt(2 * x + 1));
+}
+let fx0 = 1 / (1 + Math.sqrt(2 * a + 1));
+let fxn = 1 / (1 + Math.sqrt(2 * b + 1));
+S = h * (S + (fx0 + fxn) / 2);
+console.clear();
+console.log(S);
+*/
